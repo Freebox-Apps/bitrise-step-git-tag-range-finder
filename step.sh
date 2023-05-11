81440913783fb42cb63527a8f43570df0d1eda00
Nicolas Brocard <tchettane@gmail.com>
2023-05-11 11:23:24 +0200

    Add commit hash as output

--- step.sh
+++ step.sh
@@ -5,7 +5,15 @@ git fetch -p -P --tags --no-recurse-submodules
 
 tags=$(git tag -l "${tag_pattern}" --sort=-creatordate)
 
+commit_start=$(git show-ref -s "${tags_array[1]}")
+commit_end=$(git show-ref -s "${tags_array[0]}")
+
 echo -e $tags | while read -r -a tags_array; do
     envman add --key GIT_TAG_START --value "${tags_array[1]}"
     envman add --key GIT_TAG_END --value "${tags_array[0]}"
+    envman add --key GIT_COMMIT_START --value "${commit_start}"
+    envman add --key GIT_COMMIT_END --value "${commit_end}"
 done
+
+echo "Start commit $GIT_TAG_START ($GIT_COMMIT_START)"
+echo "End commit $GIT_TAG_END ($GIT_COMMIT_END)"
